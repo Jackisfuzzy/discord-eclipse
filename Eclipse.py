7b6fb9c2b93c4805efc70f9f492fd7251d984ad4
Eric Poroznik <ericporoznik@gmail.com>
2022-05-24 15:58:25 -0400

    Added stuff
    Added latency test

--- Eclipse.py
+++ Eclipse.py
@@ -1,14 +1,19 @@
 import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix='$')
+client=commands.Bot(command_prefix='$')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Logged on as {0}!".format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_ready():
+    print('Eclipse is ready!')   
 
-client = MyClient()
-client.run('')
+@client.command()
+async def test(ctx):
+    await ctx.send('Test Complete')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Client latency is {0}'.format(round(client.latency, 1)) + ' ms')
+
+client.run('NDg1MDU3OTYxMzU4NTI0NDI3.GLsm9B.Dgx9iqqLCIGWupr6bSRKF0OBjOgR8A5Ecp5LbI')
